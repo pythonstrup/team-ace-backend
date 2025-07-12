e301a72d67ab26158a9d11ebdfc5e77d8581951b
YIHYUN HA <hihyun16@gmail.com>
2025-07-12 14:39:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 감치왕국에 오신 것을 환영합니다. 🧚
+
+### 개발자
+
+- [pythonstrup](https://github.com/pythonstrup)
+- [hyh1016](https://github.com/hyh1016)
---
+++ src/main/java/com/nexters/teamace/TeamAceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.teamace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamAceBackendApplication.class, args);
+    }
+
+}
