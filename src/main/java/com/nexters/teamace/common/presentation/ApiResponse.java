f109e7d32b432a98f96bde7b8385bc80e8e341d5
pythonstrup <pythonstrup@gmail.com>
2025-07-20 15:36:28 +0900

    [#8] feat: GlobalExceptionHandler 작성 및 Api 스펙 수정

--- src/main/java/com/nexters/teamace/common/presentation/ApiResponse.java
+++ src/main/java/com/nexters/teamace/common/presentation/ApiResponse.java
@@ -1,12 +1,23 @@
 package com.nexters.teamace.common.presentation;
 
+import com.nexters.teamace.common.exception.ErrorType;
+
 public record ApiResponse<T>(boolean success, T data, ErrorMessage error) {
 
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>(true, data, null);
     }
 
-    public static <T> ApiResponse<T> error(String code, String message) {
-        return new ApiResponse<>(false, null, new ErrorMessage(code, message));
+    public static <T> ApiResponse<T> error(
+            final ErrorType errorType, final String message, final Object data) {
+        return new ApiResponse<>(false, null, new ErrorMessage(errorType.getCode(), message, data));
+    }
+
+    public static <T> ApiResponse<T> error(final ErrorType errorType, final String message) {
+        return error(errorType, message, null);
+    }
+
+    public static <T> ApiResponse<T> error(final ErrorType errorType) {
+        return error(errorType, errorType.getMessage(), null);
     }
 }
