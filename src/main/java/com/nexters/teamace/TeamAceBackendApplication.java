772c08285657394333e92c0a588618038704262a
pythonstrup <pythonstrup@gmail.com>
2025-07-21 19:27:41 +0900

    [#11] feat: JwtProperties 정

--- src/main/java/com/nexters/teamace/TeamAceBackendApplication.java
+++ src/main/java/com/nexters/teamace/TeamAceBackendApplication.java
@@ -2,8 +2,10 @@ package com.nexters.teamace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TeamAceBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/nexters/teamace/auth/infrastructure/jwt/JwtProperties.java
@@ -0,0 +1,6 @@
+package com.nexters.teamace.auth.infrastructure.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(String secret, long accessTokenValidity, long refreshTokenValidity) {}
